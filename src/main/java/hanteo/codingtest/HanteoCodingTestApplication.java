b9fc6dbf355e9aa2348b88ae3761c153e966034b
hanjarang <rkdsksehowl@naver.com>
2025-03-21 21:10:14 +0900

    초기설정값

---
+++ src/main/java/hanteo/codingtest/HanteoCodingTestApplication.java
@@ -0,0 +1,13 @@
+package hanteo.codingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanteoCodingTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HanteoCodingTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/hanteo/codingtest/HanteoCodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package hanteo.codingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanteoCodingTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
